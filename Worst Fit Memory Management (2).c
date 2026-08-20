4796f027d69026bb86599d2528c22665828616ac
kacharagadlapallavi <kacharagadlapallavi@gmail.com>
2026-08-20 09:23:50 +0530

    Add files via upload

---
+++ Worst Fit Memory Management (2).c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main()
+{
+    int blockSize[20], processSize[20];
+    int allocation[20];
+    int m, n;
+    int i, j, worst;
+
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &m);
+
+    printf("Enter size of each memory block:\n");
+    for (i = 0; i < m; i++)
+    {
+        printf("Block %d: ", i + 1);
+        scanf("%d", &blockSize[i]);
+    }
+
+    printf("\nEnter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter size of each process:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &processSize[i]);
+        allocation[i] = -1;
+    }
+
+    /* Worst Fit */
+    for (i = 0; i < n; i++)
+    {
+        worst = -1;
+
+        for (j = 0; j < m; j++)
+        {
+            if (blockSize[j] >= processSize[i])
+            {
+                if (worst == -1 ||
+                    blockSize[j] > blockSize[worst])
+                {
+                    worst = j;
+                }
+            }
+        }
+
+        if (worst != -1)
+        {
+            allocation[i] = worst;
+            blockSize[worst] -= processSize[i];
+        }
+    }
+
+    printf("\nProcess\tProcess Size\tBlock\n");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("P%d\t%d\t\t", i + 1, processSize[i]);
+
+        if (allocation[i] != -1)
+            printf("B%d\n", allocation[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
+
+    return 0;
+}
